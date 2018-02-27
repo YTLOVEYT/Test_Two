1bdf4b81e9dade651be2468c50bed6b2ca03caa6
TOT <824050253@qq.com>
2018-02-27 11:45:32 +0800

    第一次

---
+++ app/src/main/java/com/example/two/test/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.two.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.two.R;
+
+public class TestActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
